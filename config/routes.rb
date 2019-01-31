4a0e90256de7e96ba4e20e67876ef5fabf165c68
benjithaimmortal <benjamin.kostenbader@gmail.com>
2019-02-01 00:20:13 +0900

    added CSS and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'dynamic_pages#home'
-  get 'dynamic_pages/matchups'
-  get 'dynamic_pages/results'
+  root              'dynamic_pages#home'
+  get 'matchups' => 'dynamic_pages#matchups'
+  get 'results'  => 'dynamic_pages#results'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
