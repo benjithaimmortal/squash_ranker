16c8a1d280855a166b1010b003a7b663ba94eed7
benjithaimmortal <benjamin.kostenbader@gmail.com>
2019-03-10 21:32:23 +0900

    puma file

--- config/routes.rb
+++ config/routes.rb
@@ -9,19 +9,20 @@ Rails.application.routes.draw do
   get 'show'      => 'athletes#show'
   get 'edit'      => 'athletes#edit'
 
+  # Enable resources for the Athletes in the database,
+  # including PUT methods for incrementing Positive and Negative
+  resources :athletes do
+    member do
+      put 'rating_up', 'tossup'
+    end
+  end
+
 # Matchup!
   get 'matchup' => 'athletes#matchup'
 
 # Deprecated Results
   get 'results'  => 'dynamic_pages#results'
 
-# Enable resources for the Athletes in the database,
-# including PUT methods for incrementing Positive and Negative
-  resources :athletes do
-    member do
-      put 'rating_up', 'tossup'
-    end
-  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
